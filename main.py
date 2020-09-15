b3395e876c9b67630605ec0234c5e05993de84c5
DidierOne <dpd5518@psu.edu>
2020-09-15 15:37:56 +0000

    2 functions complete without name and main

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 # Author: Daniel Dean dpd5518@psu.edu
-# Collaborator:
-# Collaborator:
-# Collaborator:
-# Section: 1
-# Breakout: 12
-
+# Collaborator: Xiang Liu xfl5249@psu.edu
+# Collaborator: Yamin Zhang ykg5402@psu.edu
+# Collaborator: Anna Gillard amg73017@psu.edu
+# Section: 2
+# Breakout: 2
 
 def sum_n(n):
-  if n != 0:
-    pn = n-1 + n
-  n -= 1
-  print(pn)
+  if n <= 1:
+    return(n)
+  else:
+    return(n + sum_n(n - 1))
+
 def print_n(s,n):
-  print(s*n)
+  if n >= 1:
+    print(s)
+    return print_n(s,n-1)
+
+if __name__ == '__main__':
+  def run():
+    sum_n(input(""))
+
+
