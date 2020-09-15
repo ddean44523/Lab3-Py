0c3f232b412b84317ab0ee4e8769ab64cb2d34ac
DidierOne <dpd5518@psu.edu>
2020-09-15 14:43:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Author: Daniel Dean dpd5518@psu.edu
+# Collaborator:
+# Collaborator:
+# Collaborator:
+# Section: 1
+# Breakout: 12
+
+
+def sum_n(n):
+  if n != 0:
+    pn = n-1 + n
+  n -= 1
+  print(pn)
+def print_n(s,n):
+  print(s*n)
